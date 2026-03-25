fb2265a703d7719cc772818a344e928e9ed63249
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2026-03-25 19:57:04 -0400

    LiveSplit.RainbowDetailedTimer

---
+++ components/LiveSplit.RainbowDetailedTimer/src/LiveSplit.RainbowDetailedTimer/UI/Components/RainbowDetailedTimerFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using LiveSplit.Model;
+using LiveSplit.UI.Components;
+
+[assembly: ComponentFactory(typeof(RainbowDetailedTimerFactory))]
+
+namespace LiveSplit.UI.Components;
+
+public class RainbowDetailedTimerFactory : IComponentFactory
+{
+    public string ComponentName => "Rainbow Detailed Timer";
+
+    public string Description => "Displays the run timer, segment timer, and segment times for up to two comparisons. Turns rainbow when you PB!";
+
+    public ComponentCategory Category => ComponentCategory.Timer;
+
+    public IComponent Create(LiveSplitState state)
+    {
+        return new RainbowDetailedTimer(state);
+    }
+
+    public string UpdateName => ComponentName;
+
+    public string XMLURL => "";
+
+    public string UpdateURL => "";
+
+    public Version Version => Version.Parse("1.0.0");
+}
