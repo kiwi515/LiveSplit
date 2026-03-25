35018abf5ea2c09553156439f62fe17691262081
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2026-03-25 19:56:44 -0400

    LiveSplit.RainbowTimer

---
+++ components/LiveSplit.RainbowTimer/src/LiveSplit.RainbowTimer/UI/Components/RainbowTimerFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using LiveSplit.Model;
+using LiveSplit.UI.Components;
+
+[assembly: ComponentFactory(typeof(RainbowTimerFactory))]
+
+namespace LiveSplit.UI.Components;
+
+public class RainbowTimerFactory : IComponentFactory
+{
+    public string ComponentName => "Rainbow Timer";
+
+    public string Description => "Displays the current run time. Turns rainbow when you PB!";
+
+    public ComponentCategory Category => ComponentCategory.Timer;
+
+    public IComponent Create(LiveSplitState state)
+    {
+        return new RainbowTimer();
+    }
+
+    public string UpdateName => ComponentName;
+
+    public string XMLURL => "";
+
+    public string UpdateURL => "";
+
+    public Version Version => Version.Parse("1.0.0");
+}
